381f3460b4de374fb557612bbfb257b590e581a0
Sina Sadeghi <sina.sadeghi83@gmail.com>
2025-10-24 23:28:00 +0330

    :wrench: :hammer: Add env variables to viper

--- config/config.go
+++ config/config.go
@@ -3,12 +3,18 @@ package config
 import (
 	"context"
 
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
 func Configure(ctx context.Context) error {
+	godotenv.Load()
+
 	viper.SetConfigName("config")
+	viper.SetConfigType("json")
 	viper.AddConfigPath(".")
+
+	viper.AutomaticEnv()
 
 	err := viper.ReadInConfig()
 	if err != nil {
