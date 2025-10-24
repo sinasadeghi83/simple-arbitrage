08bd09b8f6e597c8bca80113469efe911edbabd1
Sina Sadeghi <sina.sadeghi83@gmail.com>
2025-10-24 22:33:19 +0330

    :sparkles: Add telegram bot

--- main.go
+++ main.go
@@ -3,24 +3,54 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 	"time"
 
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
 	"github.com/sinasadeghi83/simple-arbitrage/arbitrage"
+	"github.com/sinasadeghi83/simple-arbitrage/config"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
+	err := config.Configure(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	b, err := bot.New(viper.GetString("bot.token"), opts...)
+	if err != nil {
+		panic(err)
+	}
 
 	arbChan := make(chan arbitrage.ArbDetail)
-	go arbitrage.FindPeriodicly(ctx, "TONIRT", time.Second, arbChan)
+	pairs := viper.GetStringSlice("arbitrage.pairs")
+	for _, pair := range pairs {
+		go arbitrage.FindPeriodicly(ctx, pair, time.Second, arbChan)
+	}
 	go func() {
 		for ctx.Err() == nil {
 			arbDetail := <-arbChan
+			b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID: viper.GetString("bot.channel_id"),
+				Text: fmt.Sprintf("New arbitrage opportunity found:\n Pair: %s\n Src: %s\n Dest: %s\n Amount: %s\n Ask: %s\n Bid: %s",
+					arbDetail.Pair, arbDetail.Src, arbDetail.Dest, arbDetail.Amount, arbDetail.Ask, arbDetail.Bid),
+			})
 			fmt.Println(arbDetail)
 		}
 	}()
-	<-ctx.Done()
 
-	fmt.Print("Done")
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+
 }
